e2e638035c68a77b268a34b59982a8cb02ea7faa
Zurieel <zurielmtz1908@ciencias.unam.mx>
2024-10-11 15:55:04 -0600

    UPDATE: Termine la maquetacion de las vistas login y registro, faltaria conectarlas con el back. Estoy empezando la maquetacion de la vista home

---
+++ habitos/views.py
@@ -0,0 +1,67 @@
+from django import forms
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+
+"""
+Apartado para la vista login
+"""
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label='Usuario')
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('home')
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+
+"""
+Apartado para la vista register
+"""
+class CustomUserCreationForm(forms.ModelForm):
+
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+        labels = {
+            'username': 'Usuario',
+            'first_name': 'Nombre',
+            'last_name': 'Apellido',
+            'email': 'Correo',
+        }
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('home') 
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+"""
+Aparatado para la vista home
+"""
+def home_view(request):
+    return render(request, 'home.html')
