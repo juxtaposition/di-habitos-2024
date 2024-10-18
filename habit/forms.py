a336e4bb60fd9f726f27a4aa212a39fca267390d
Zurieel <zurielmtz1908@ciencias.unam.mx>
2024-10-18 03:42:47 -0600

    UPDATE; Ya casi termino home_list, solo falta la funcionalidad del boton editar habito

--- habit/forms.py
+++ habit/forms.py
@@ -2,16 +2,27 @@ from django import forms
 from .models import Habit, Category
 
 class HabitForm(forms.ModelForm):
-    category = forms.ChoiceField(choices=[])
+    class Meta:
+        model = Habit
+        fields = ['name', 'description', 'frequency', 'category']
+        labels = {
+            'name': 'Nombre',
+            'description': 'Descripción',
+            'frequency': 'Frecuencia',
+            'category': 'Categoría',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control-modal', 'placeholder': 'Ingrese el nombre del hábito'}),
+            'description': forms.Textarea(attrs={'class': 'form-control-modal', 'rows': 2, 'placeholder': 'Describa su hábito'}),
+            'frequency': forms.Select(attrs={'class': 'form-control-modal', 'style':'background-color:white'}),
+        }
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['category'].choices = self.get_category_choices()
+        self.fields['category'] = forms.ModelChoiceField(
+            queryset=Category.objects.all(),
+            label='Categoría',
+            widget=forms.Select(attrs={'class': 'form-control-modal','style':'background-color:white'})
+        )
 
-    def get_category_choices(self):
-        categories = Category.objects.all()
-        return [(category.id, category.name) for category in categories]
-
-    class Meta:
-        model = Habit
-        fields = ['name', 'description', 'frequency']
