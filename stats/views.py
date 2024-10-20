e9aabf6c7523ab17bf2318eb5326bcbe1952b9ae
= <exbios@live.co.uk>
2024-10-19 22:35:04 -0600

    Added simple examples stats

--- stats/views.py
+++ stats/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
-from django.template.loader import render_to_string
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from habit.models import Habit, Category
@@ -8,42 +7,35 @@ from django.utils.dateparse import parse_date
 from django.db.models import Sum, Count, Q
 import json
 from datetime import datetime
-
+from collections import Counter
 
 @login_required
 def estadisticas(request):
+
     usuario = request.user
 
-    fecha_inicio = request.GET.get('fecha_inicio')
-    fecha_fin = request.GET.get('fecha_fin')
-    category_id = request.GET.get('categoria')
+    fecha_inicio   = request.GET.get('fecha_inicio')
+    fecha_fin      = request.GET.get('fecha_fin')
+    category_id    = request.GET.get('categoria')
+    categories     = Category.objects.all()
+    category_names = []
 
     if not (category_id is None):
-        print('noes none')  
-        habits = Habit.objects.filter(user_id=usuario, category_id=category_id)
+        habits = Habit.objects.all().filter(user_id=usuario, category_id=category_id)
     else:
-        habits = Habit.objects.filter(user_id=usuario)
+        habits = Habit.objects.all().filter(user_id=usuario)
 
-    categoryByUser = habits.values('category_id').annotate(total_category=Count('category_id'))
- 
-    print(categoryByUser)
-    for c in categoryByUser:
-        if c.get('category_id') == 1:
-            print('Hogar')
-        elif c.get('category_id') == 2:
-            print('s')
-        elif c.get('category_id') == 2:
-             print('v')
-        elif c.get('category_id') == 2:
-             print('ss')
-        else:
-             print('aa')
+    if fecha_inicio and fecha_fin:
+        habits = habits.filter(created_at__range=[parse_date(fecha_inicio), parse_date(fecha_fin)])
 
-   
+    # this is better way to get the all categories by count
+    for h in habits:
+        category_names.append(h.category.name)
+    
+    allCategories = dict(Counter(category_names))
 
-    # if fecha_inicio and fecha_fin:
-    #     compras = compras.filter(fecha__range=[parse_date(fecha_inicio), parse_date(fecha_fin)])
 
+    print(allCategories)
     # if categorias:
     #     compras = compras.filter(fruta__categorias__id__in=categorias).distinct()
 
@@ -63,4 +55,7 @@ def estadisticas(request):
     #     'frutas_compradas': json.dumps(list(frutas_compradas)),
     #     'todas_categorias': todas_categorias,
     # }
-    return render(request, 'stats/index.html', { 'habits': json.dumps(({}), indent=2) })
+    return render(request, 'stats/index.html', { 
+        'habits': json.dumps(allCategories),
+         "categories": categories
+        })
